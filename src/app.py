7d63bb4bb71227b098d33f1e5505f41e7de95969
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-16 22:22:07 -0300

    0.0.1

--- src/app.py
+++ src/app.py
@@ -56,7 +56,7 @@ class CrossPlatformApp:
                                 text="↵",
                                 command=self.send_message,
                                 width=button_width,
-                                foreground="rgba(56, 96, 175, 0.24)",
+                                foreground="#b5bec7",
                                 borderwidth=0)
         send_button.grid(row=0, column=0, padx=2)
 

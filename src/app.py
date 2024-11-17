3401833b9263ea47c055303c843e93d822036ae8
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-17 01:27:39 -0300

    0.0.1

--- src/app.py
+++ src/app.py
@@ -41,7 +41,7 @@ class CrossPlatformApp:
     def generate_icon_image(self):
         """Gera um ícone simples usando PIL."""
         size = (64, 64)
-        image = Image.new("RGB", size, color="lightblue")
+        image = Image.new("RGB", size, color="blue")
         draw = ImageDraw.Draw(image)
         # simbolo parecido com o simbolo do google assistant
         draw.rectangle((16, 16, 48, 48))

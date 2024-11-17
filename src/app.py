28f9ba2928183fce90b8ec6cdca0664d902b4889
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-17 01:31:43 -0300

    0.0.1

--- src/app.py
+++ src/app.py
@@ -44,7 +44,7 @@ class CrossPlatformApp:
         image = Image.new("RGB", size)
         draw = ImageDraw.Draw(image)
         # simbolo parecido com o simbolo do google assistant
-        draw.ellipse((32, 32, 32, 32), fill="orange")
+        draw.ellipse((32, 32, 32, 32), fill="#4d7aa1")
         draw.ellipse((24, 24, 40, 40), fill="orange")
         draw.ellipse((16, 16, 48, 48), fill="blue", outline="black")
         return image

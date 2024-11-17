1da5792d5c383b97dd5712c269f453f33bbd8205
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-16 22:45:07 -0300

    0.0.1

--- src/app.py
+++ src/app.py
@@ -102,7 +102,7 @@ class CrossPlatformApp:
                                   highlightcolor="#252525",
                                   highlightbackground="#252525",
                                   highlightthickness=0,
-                                  borderwidth=1,
+                                  borderwidth=2,
                                   relief="groove",
                                   insertbackground="white",
                                   state=tk.NORMAL)

8428e854ade2fa0bb590a4eed596d50109fb261a
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-12-13 01:29:38 -0300

    0.0.1

---
+++ src/test.py
@@ -0,0 +1,39 @@
+import base64
+import vertexai
+from vertexai.generative_models import GenerativeModel, SafetySetting, Part
+
+
+def multiturn_generate_content():
+    vertexai.init(project="hcloud-321513", location="southamerica-east1")
+    model = GenerativeModel(
+        "gemini-1.0-pro-002",
+    )
+    chat = model.start_chat()
+
+
+generation_config = {
+    "max_output_tokens": 2048,
+    "temperature": 1,
+    "top_p": 1,
+}
+
+safety_settings = [
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold=SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    ),
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold=SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    ),
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold=SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    ),
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold=SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    ),
+]
+
+multiturn_generate_content()

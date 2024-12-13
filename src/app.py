6eb0948c2b8cd015a5e63758104eb0ea60ce9e9a
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-12-13 00:12:29 -0300

    0.0.1

--- src/app.py
+++ src/app.py
@@ -33,23 +33,23 @@ class App(Gtk.Window):
         
         # Bottom
         bottom = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
-        layout.pack_start(bottom, False, False, 0)
+        layout.pack_start(bottom, False, False, 10)
 
         # Campo de entrada
         self.entry = Gtk.Entry()
         self.entry.set_placeholder_text("Digite sua mensagem...")
         self.entry.connect("activate", self.on_message_sent)
-        bottom.pack_start(self.entry, True, True, 10)
+        bottom.pack_start(self.entry, True, True, 0)
 
         # Botão de enviar
         send_button = Gtk.Button(label="Enviar")
         send_button.connect("clicked", self.on_message_sent)
-        bottom.pack_start(send_button, False, False, 10)
+        bottom.pack_start(send_button, False, False, 0)
 
         # Botão para abrir configurações ao lado do botão enviar
         settings_button = Gtk.Button(label="⚙")
         settings_button.connect("clicked", self.open_settings)
-        bottom.pack_start(settings_button, False, False, 10)
+        bottom.pack_start(settings_button, False, False, 0)
 
     def on_message_sent(self, widget):
         # Captura o texto da entrada

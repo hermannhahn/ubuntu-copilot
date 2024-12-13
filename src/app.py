3887906aecb6acbdd49e4235876dda6a7cbd7dd2
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-12-13 18:15:33 -0300

    0.0.1

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 import gi
 
-gi.require_version("Gtk", "3.0")
+gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk
 from ui.chat import ChatWindow
 
 class App(Gtk.Window):
     def __init__(self):
-        super().__init__(title="Linux Co-Pilot Chatbot")
+        super().__init__(title="Ubuntu Co-Pilot Chatbot")
         self.set_default_size(600, 400)
 
         # Layout principal (chat)

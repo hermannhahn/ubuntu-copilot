bea9ae57dd01bf71972bd87cee46f3f7fc23cf44
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-12-13 15:31:19 -0300

    0.0.1

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+from ui.chat import ChatWindow
 
 class App(Gtk.Window):
     def __init__(self):
@@ -9,7 +10,8 @@ class App(Gtk.Window):
         self.set_default_size(600, 400)
 
         # Layout principal (chat)
-
+        self.chat_window = ChatWindow()
+        self.add(self.chat_window)
 
 if __name__ == "__main__":
     app = App()

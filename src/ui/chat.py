4f2bf66a616dfc588f431735754da895ee6f8b48
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-12-13 23:38:10 -0300

    0.0.1

--- src/ui/chat.py
+++ src/ui/chat.py
@@ -103,12 +103,12 @@ class ChatWindow:
         buffer = self.chat_display.get_buffer()
         buffer.insert(buffer.get_end_iter(), f"Bot: {response}\n")
 
-    def open_settings(self, widget):
+    def open_settings(self):
         # Abre a janela de configurações
         self.settings_window = SettingsWindow()
         self.settings_window.show()
 
     def close_alert(self, d):
         d.close()
-        self.open_settings(None)
+        self.open_settings()
 
